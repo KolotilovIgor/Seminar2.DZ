8480824cca50c1a436764211d5693d9f1c65b59c
IgorK <gosha424@mail.ru>
2023-12-30 20:00:20 +0300

    Задача2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая принимает
+//на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠
+//0 и выдаёт номер координатной четверти плоскости,
+//в которой находится эта точка.
+Console.WriteLine("Введите число X");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число Y");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > 0 && num2 > 0)
+{
+    Console.WriteLine("1");
+}
+else
+{
+    if (num1 < 0 && num2>0)
+    {
+       Console.WriteLine("2");
+    }
+    else
+    {
+      if (num1 < 0 && num2<0)
+      {
+        Console.WriteLine("3");
+      }  
+      else
+      {
+             if (num1 > 0 && num2<0)
+      {
+        Console.WriteLine("4");
+      }  
+      }
+    }
+}
